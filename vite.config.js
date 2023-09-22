d86bf6cf3ab787b15f8e8d582c6fdac845ae79e0
Rinat Chubarov <r.pettrovich@gmail.com>
2023-09-23 00:59:01 +0600

    Configuring Vite to handle English version of index.html

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,25 @@
-//settings
-export default {
+// Settings
+import {defineConfig} from 'vite';
+import {resolve} from 'path';
+
+export default defineConfig({
     base: '/lab',
     assetsInclude: ['**/*.glb', '**/*.hdr'],
     build:
     {
         chunkSizeWarningLimit: 2000,
+        rollupOptions:
+        {
+            input:
+            {
+                main: resolve(__dirname, 'index.html'),
+                nested: resolve(__dirname, 'en/index.html'),
+            },
+        },
     },
     server:
     {
         host: true,
         open: true,
     }
-}
+});
